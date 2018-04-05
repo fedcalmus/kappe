3f12cbf126eda3f05c59602828477413ad1bbb45
fedcalmus <fedcalmus@gmail.com>
2018-04-05 16:39:29 +0400

    js one file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("hideFilterSection") == -1) {
-        x.className += " hideFilterSection";
-    } else {
-        x.className = x.className.replace(" hideFilterSection", "");
-    }
-}
+$(function() {
+
+    var AppPlugins = {
+
+        /* Scrollbar */
+        scrollbar: function(){
+            $(window).on("load",function(){
+                $(".leftSideSection").mCustomScrollbar();
+            });
+        },
+
+
+        /* Accordion Effect */
+        accordionEffect: function(toggle, element, className) {
+
+            var filterToggle = $(toggle),
+                filterSection = $(element),
+                className = className;
+
+
+            filterToggle.on("click", function () {
+                filterSection.toggleClass(className);
+            });
+        }
+    };
+
+    /* ------ Plugins Init ------ */
+    AppPlugins.scrollbar();
+    AppPlugins.accordionEffect(".menuToggle", ".leftSideSection", "showLeftSideContent");
+    AppPlugins.accordionEffect(".filterToggle", ".filterSection", "hideFilterSection");
+});
--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-(function($){
-    $(window).on("load",function(){
-        $(".leftSideSection").mCustomScrollbar();
-    });
-})(jQuery);/**
- * Created by STDev031 on 3/23/2018.
- */
